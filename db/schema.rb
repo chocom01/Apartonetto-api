4bedabc3db863cb93aff40d8fc174d6827e5d90d
chocom01 <mukola0101@gmail.com>
2021-01-28 16:56:35 +0200

    schema and seeds

---
+++ db/schema.rb
@@ -0,0 +1,100 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_27_125512) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "payment_id"
+    t.bigint "tenant_id"
+    t.bigint "property_id"
+    t.integer "status", default: 0
+    t.datetime "start_rent_at"
+    t.datetime "end_rent_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["payment_id"], name: "index_bookings_on_payment_id"
+    t.index ["property_id"], name: "index_bookings_on_property_id"
+    t.index ["tenant_id"], name: "index_bookings_on_tenant_id"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.bigint "booking_id"
+    t.integer "tenant_unread_messages_count"
+    t.integer "provider_unread_messages_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_chats_on_booking_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "chat_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.bigint "tenant_id"
+    t.bigint "provider_id"
+    t.bigint "booking_id"
+    t.string "service"
+    t.string "info"
+    t.integer "amount"
+    t.integer "status", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_payments_on_booking_id"
+    t.index ["provider_id"], name: "index_payments_on_provider_id"
+    t.index ["tenant_id"], name: "index_payments_on_tenant_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.bigint "provider_id"
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider_id"], name: "index_properties_on_provider_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "reviewer_id"
+    t.string "reviewable_type"
+    t.bigint "reviewable_id"
+    t.integer "rate"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable_type_and_reviewable_id"
+    t.index ["reviewer_id"], name: "index_reviews_on_reviewer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create(
+  [
+    { first_name: 'Kolya', last_name: 'Vasyliv', phone: '380631775050',
+      email: 'qweasd1@gmail.com', password: 'qweasd' },
+    { first_name: 'Igor', last_name: 'Kuzmin', phone: '380631775051',
+      email: 'qweasd2@gmail.com', password: 'rtyfgh' },
+    { first_name: 'Nazar', last_name: 'Ivleev', phone: '380631775052',
+      email: 'qweasd3@gmail.com', password: 'zxcvbn' },
+    { first_name: 'Taras', last_name: 'Kravchyk', phone: '380631775053',
+      email: 'qweasd4@gmail.com', password: 'vbndfg' }
+  ]
+)
+properties = Property.create(
+  [
+    { name: 'Apartament', description: 'Smart apartment',
+      price: 500, provider_id: 1, location: 'Lviv' },
+    { name: 'Apartament', description: 'Luxury apartament',
+      price: 400, provider_id: 1, location: 'Lviv' },
+    { name: 'Apartament', description: 'Сomfortable apartment ',
+      price: 300, provider_id: 1, location: 'Lviv' },
+    { name: 'Apartament', description: 'Low cost apartament',
+      price: 150, provider_id: 1, location: 'Lviv' },
+    { name: 'Apartament', description: 'Small apartament',
+      price: 200, provider_id: 1, location: 'Lviv' },
+    { name: 'House', description: 'Big house',
+      price: 1500, provider_id: 1, location: 'Lviv oblast' }
+  ]
+)
+booking = Booking.create(
+  [
+    { tenant_id: 2, property_id: 4, start_rent_at: '01.01.21 ', end_rent_at: '02.01.21' },
+    { tenant_id: 3, property_id: 3, start_rent_at: '01.01.21', end_rent_at: '02.01.21' },
+    { tenant_id: 4, property_id: 3, start_rent_at: '01.01.21', end_rent_at: '02.01.21' }
+  ]
+)
+reviews = Review.create(
+  [
+    { reviewer_id: 2, reviewable: Property.find(4),
+      rate: 3, text: 'Normal apartment' },
+    { reviewer_id: 3, reviewable: Property.find(3),
+      rate: 4, text: 'comfortable enough ' },
+    { reviewer_id: 4, reviewable: Property.find(3),
+      rate: 4, text: 'comfortable enough ' }
+  ]
+)
