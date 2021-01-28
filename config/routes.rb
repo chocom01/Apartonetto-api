bccc203bfb4a31ed776756fa08a5f4a5c6c666ef
chocom01 <mukola0101@gmail.com>
2021-01-28 16:56:23 +0200

    routes for sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post 'sign_in' => 'user_token#sign_in'
+  post 'sign_up' => 'user_token#sign_up'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
