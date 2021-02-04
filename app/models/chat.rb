f38f1629ddd25a0013b325b92bddf390b062331e
chocom01 <mukola0101@gmail.com>
2021-02-04 18:51:47 +0200

    change chat model and migrate

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,4 +3,12 @@
 class Chat < ApplicationRecord
   belongs_to :booking
   has_many :messages
+
+  validate :true_booking
+
+  private
+
+  def true_booking
+    booking_id == booking.id || errors.add(:booking, 'invalid booking')
+  end
 end
--- db/migrate/20210127102803_create_chats.rb
+++ db/migrate/20210127102803_create_chats.rb
@@ -4,8 +4,8 @@ class CreateChats < ActiveRecord::Migration[6.0]
   def change
     create_table :chats do |t|
       t.belongs_to :booking
-      t.integer :tenant_unread_messages_count
-      t.integer :provider_unread_messages_count
+      t.integer :tenant_unread_messages_count, default: 0
+      t.integer :provider_unread_messages_count, default: 0
 
       t.timestamps
     end
