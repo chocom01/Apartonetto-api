c72bb050c3574a112021dfa12aea2212bd01396a
chocom01 <mukola0101@gmail.com>
2021-02-04 18:51:07 +0200

    add method for render errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  skip_before_action :verify_authenticity_token
+
+  private
+
+  def render_errors(errors, status = :unprocessable_entity)
+    render json: { errors: errors }, status: status
+  end
 end
