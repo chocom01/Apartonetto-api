0a1a072b4a74c1a76d4028b2a945aa95c9a16968
chocom01 <mukola0101@gmail.com>
2021-02-09 18:36:35 +0200

    update property and chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,6 +2,8 @@
 
 class Chat < ApplicationRecord
   belongs_to :booking
+  belongs_to :tenant, class_name: 'User'
+  belongs_to :provider, class_name: 'User'
   has_many :messages
 
   validate :true_booking
--- app/models/property.rb
+++ app/models/property.rb
@@ -11,19 +11,8 @@ class Property < ApplicationRecord
   validates :price, numericality: { greater_than: 0, less_than: 10_000 }
   validate :true_provider_role
 
-  # scope :by_name, ->(name) { where('name like ?', "#{name}%") }
-  # scope :by_user, ->(user_id) { where(user_id: user_id) }
-  # scope :by_category, ->(category_id) { where(category_id: category_id) }
-  # scope :by_options, lambda { |option_ids|
-  #   joins(:options).where(options: { id: option_ids })
-  #                  .having("count(option_id) = #{option_ids.size}")
-  #                  .group(:id)
-  # }
-  # scope :price_min, ->(number) { where(arel_table[:price].gt(number)) }
-  # scope :price_max, ->(number) { where(arel_table[:price].lt(number)) }
+  private
 
-  # private
-  #
   def true_provider_role
     provider.role == 'provider' || provider.role == 'admin' ||
       errors.add(:provider, 'must be provider or admin')
