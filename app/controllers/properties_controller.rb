dbd6b08e61648d8bc7dd2adea1057b93f3c6bd8a
chocom01 <mukola0101@gmail.com>
2021-02-09 18:36:55 +0200

    update property controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,7 +2,6 @@
 
 class PropertiesController < ApplicationController
   before_action :load_property, only: %i[update destroy]
-  # before_action :authenticate_user, only: %i[create]
 
   def index
     properties = Property.all
@@ -44,11 +43,4 @@ class PropertiesController < ApplicationController
   def load_property
     @property = Property.find_by(provider: current_user, id: params[:id]) || head(:not_found)
   end
-
-  # def filtering_params
-  #   params.permit(
-  #     :by_city, :by_name, :by_category, :by_user, :price_min, :price_max,
-  #     :by_options
-  #   )
-  # end
 end
