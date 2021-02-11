2ef9f563897e1bf0148c9681321a1958afa207b8
chocom01 <mukola0101@gmail.com>
2021-02-11 17:18:56 +0200

    beauty changes payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,58 +2,52 @@
 
 class PaymentsController < ApplicationController
   before_action :load_payment, only: %i[show pay reject]
-  before_action :load_role, only: %i[index]
-  before_action :true_role, only: %i[pay reject]
+  before_action :right_role_for_status_update, only: %i[pay reject]
 
   def index
-    render json: payment
+    load_payments_by_role
+    render json: @payments
   end
 
   def show
-    render json: payment
+    render json: @payment
   end
 
   def pay
-    if payment.waiting_for_payment? || payment.rejected?
-      render json: payment if payment.paid!
-    else
-      payment.errors.add(:payment, 'you already paid')
-      render_errors(payment.errors)
-    end
+    check_status || (render json: @payment if @payment.paid!)
   end
 
   def reject
-    if payment.waiting_for_payment? || payment.rejected?
-      render json: payment if payment.rejected!
-    else
-      payment.errors.add(:status, 'paid')
-      render_errors(payment.errors)
-    end
+    check_status || (render json: @payment if @payment.rejected!)
   end
 
   private
 
-  attr_reader :payment
+  def check_status
+    @payment.paid? &&
+      @payment.errors.add(:payment, 'you paid') &&
+      (render json: { errors: @payment.errors }, status: :unprocessable_entity)
+  end
 
-  def true_role
+  def right_role_for_status_update
     if current_user.role != 'tenant'
-      payment.errors.add(:status, "provider can't change status payment")
-      render_errors(payment.errors)
+      @payment.errors.add(:status, "provider can't change status payment")
+      render_errors(@payment.errors)
     end
   end
 
-  def load_role
+  def load_payments_by_role
     case current_user.role
     when 'provider'
-      @payment = current_user.received_payments
+      @payments = current_user.received_payments
     when 'tenant'
-      @payment = current_user.sent_payments
+      @payments = current_user.sent_payments
     else
       head(:not_found)
     end
   end
 
   def load_payment
-    @payment = load_role.find_by(id: params[:id]) || head(:not_found)
+    @payment = load_payments_by_role.find(params[:id])
   end
 end
