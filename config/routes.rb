28b987968a314268fd8fa0d4bd7aa506d9a73cea
chocom01 <mukola0101@gmail.com>
2021-02-11 17:15:01 +0200

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
 
   resources :properties, only: %i[index show create update destroy]
 
-  resources :bookings, only: %i[index show create update destroy]
+  resources :bookings, only: %i[index show create update]
   patch '/bookings/:id/cancel', to: 'bookings#cancel'
   patch '/bookings/:id/confirm', to: 'bookings#confirm'
   patch '/bookings/:id/declin', to: 'bookings#declin'
 
-  resources :payments, only: %i[index show create update destroy]
+  resources :payments, only: %i[index show]
   patch '/payments/:id/pay', to: 'payments#pay'
   patch '/payments/:id/reject', to: 'payments#reject'
 
