d37b63f7494b6e610e975d7dc32e504229c8e22e
chocom01 <mukola0101@gmail.com>
2021-02-11 17:19:51 +0200

    deleted useless validate

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -7,22 +7,9 @@ class Payment < ApplicationRecord
   belongs_to :payer, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
 
-  validate :true_user
-  validate :true_amount
   validate :true_booking
 
   private
-
-  def true_user
-    booking.property.provider == recipient && booking.tenant == payer ||
-      errors.add(:booking, 'failed role')
-  end
-
-  def true_amount
-    booking.property.price *
-      (booking.end_rent_at - booking.start_rent_at).to_i == amount ||
-      errors.add(:amount, 'must be price property')
-  end
 
   def true_booking
     booking_id == booking.id || errors.add(:booking, 'failed booking')
