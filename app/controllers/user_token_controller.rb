75df9118f5d1495cdd47ff156cfefa797ae13793
chocom01 <mukola0101@gmail.com>
2021-02-15 14:02:14 +0200

    rename action "sign_up" to "create" and replace it to user controller

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -2,21 +2,10 @@
 
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
-  before_action :authenticate, only: :sign_in
+  before_action :authenticate
+
   def sign_in
     render json: token_json_hash(entity)
-  end
-
-  def sign_up
-    user = User.new(user_params)
-    user.errors.add(:role, "can't create admin")
-    if user.role == 'admin'
-      render json: { errors: user.errors }, status: :unprocessable_entity
-    elsif user.save
-      render json: token_json_hash(user)
-    else
-      render json: { errors: user.errors }, status: :unprocessable_entity
-    end
   end
 
   private
@@ -26,11 +15,5 @@ class UserTokenController < Knock::AuthTokenController
       jwt: "Bearer #{Knock::AuthToken.new(payload: { sub: user.id }).token}",
       payload: user.as_json
     }
-  end
-
-  def user_params
-    params.require(:user).permit(
-      :first_name, :last_name, :phone, :email, :password, :role
-    )
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,43 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: %i[update]
+
+  def create
+    user = User.new(user_params_for_create)
+    user.errors.add(:role, "can't create admin")
+    if user.role == 'admin'
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    elsif user.save
+      render json: token_json_hash(user)
+    else
+      render json: { errors: user.errors }, status: :unprocessable_entity
+    end
+  end
 
   def update
     return render_errors(current_user.errors) unless
-      current_user.update(user_params)
+      current_user.update(user_params_for_update)
 
     render json: current_user
   end
 
   private
 
-  def user_params
+  def token_json_hash(user)
+    {
+      jwt: "Bearer #{Knock::AuthToken.new(payload: { sub: user.id }).token}",
+      payload: user.as_json
+    }
+  end
+
+  def user_params_for_create
+    params.require(:user).permit(
+      :first_name, :last_name, :phone, :email, :password, :role
+    )
+  end
+
+  def user_params_for_update
     params.require(:user)
           .permit(:first_name, :last_name, :phone, :email, :password)
   end
