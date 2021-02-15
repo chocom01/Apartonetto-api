b7777d186550ac5bc2492eb1ed7f41770c1b3555
chocom01 <mukola0101@gmail.com>
2021-02-15 13:38:17 +0200

    add gem pundit and settings

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'puma', '~> 4.1'
 gem 'jbuilder', '~> 2.7'
 gem 'kaminari'
 gem 'knock'
+gem 'pundit'
 gem 'rspec_api_documentation'
 gem 'sidekiq'
 # Use Redis adapter to run Action Cable in production
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  include Pundit
 
   private
 
   def render_errors(errors, status = :unprocessable_entity)
     render json: { errors: errors }, status: status
+  end
+
+  rescue_from Pundit::NotAuthorizedError do |e|
+    render_errors(e.message, :forbidden)
   end
 end
