840c5ed510ff1384beda0a547b9b6f9b2daf77c1
chocom01 <mukola0101@gmail.com>
2021-02-25 13:45:22 +0200

    create photos_controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,18 @@
+class PhotosController < ApplicationController
+
+  def index
+    photos = PhotoBlueprint.render Photo.all
+    render json: photos
+  end
+
+  def create
+    photo = PhotoBlueprint.render Photo.create(photo_params)
+    render json: photo
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :property_id)
+  end
+end
