646814d891997311b0e4f0ea1207d698ddfd7e62
chocom01 <mukola0101@gmail.com>
2021-03-22 17:40:51 +0200

    beauty changes and new scope by_coordinates

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class Property < ApplicationRecord
-  paginates_per 10
   belongs_to :provider, class_name: 'User'
   has_many :reviews, as: :reviewable
   has_many :bookings, dependent: :destroy
   has_many :photos
 
-  validates :name, :location, :address, length: { in: 4..20 }
+  validates :name, :address, length: { in: 4..20 }
   validates :description, presence: true
   validates :price, numericality: { greater_than: 0, less_than: 10_000 }
   validates :minimum_days, numericality: { greater_than: 0 }
-  validates :guests_capacity, numericality: { greater_than: 1, less_than: 15 }
+  validates :guests_capacity, numericality: { greater_than: 0, less_than: 15 }
   validates :rooms_count, numericality: { greater_than: 0, less_than: 12 }
 
   scope :min_price, ->(amount) { where('price >= ?', amount) }
@@ -27,10 +26,20 @@ class Property < ApplicationRecord
     where('lower(address) like ?', "%#{address.downcase}%")
   }
 
+  scope :by_coordinates, lambda {
+    |longitude_down, longitude_up, latitude_left, latitude_right|
+    where(
+      'longitude BETWEEN :bottom_point AND :top_point AND
+      latitude BETWEEN :left_point AND :right_point',
+      bottom_point: longitude_down, top_point: longitude_up,
+      left_point: latitude_left, right_point: latitude_right
+    )
+  }
+
   scope :vacant_properties_in_dates, lambda { |initial_date, finale_date|
     booking_table = Booking.arel_table
     property_table = Property.arel_table
-    Property.find_by_sql(
+    find_by_sql(
       property_table.join(booking_table, Arel::Nodes::OuterJoin)
                     .on(property_table[:id].eq(booking_table[:property_id])
                     .and(booking_table[:end_rent_at].gt(initial_date))
